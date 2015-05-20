570b94ee478f8c6b1ae7ca510c9ecdede3bffac7
jorgeluis11 <perez.jorge718@gmail.com>
2015-05-19 22:21:44 -0400

    add rating and some PEP8 fix

--- motels/models.py
+++ motels/models.py
@@ -2,8 +2,6 @@ from autoslug import AutoSlugField
 from core.models import TimeStampedModel
 from django.db import models
 
-from towns.models import Town
-
 from versatileimagefield.fields import PPOIField
 from versatileimagefield.fields import VersatileImageField
 
@@ -12,10 +10,13 @@ class Motel(TimeStampedModel):
     town = models.ForeignKey('towns.Town', related_name='town')
     name = models.CharField(max_length=50, unique=False)
     slug = AutoSlugField(populate_from='name', unique=True, max_length=50)
-    latitude = models.DecimalField(max_digits=16, decimal_places=13, blank=True, null=True)
-    longitude = models.DecimalField(max_digits=16, decimal_places=13, blank=True, null=True)
+    latitude = models.DecimalField(max_digits=16, decimal_places=13,
+        blank=True, null=True)
+    longitude = models.DecimalField(max_digits=16, decimal_places=13,
+        blank=True, null=True)
     price_range = models.IntegerField(blank=True, null=True)
     rating = models.FloatField(editable=False, default=0)
+    user_count = models.IntegerField(editable=False, default=0)
     address = models.CharField(max_length=255, blank=True, null=True)
     address2 = models.CharField(max_length=255, blank=True, null=True)
     telephone = models.CharField(
@@ -24,21 +25,21 @@ class Motel(TimeStampedModel):
     website = models.URLField(max_length=200, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     status = models.BooleanField(default=True)
-    amenities = models.ManyToManyField('motels.Amenitie', related_name='amenities',
-        blank=True)
+    amenities = models.ManyToManyField('motels.Amenitie',
+        related_name='amenities', blank=True)
 
     def __unicode__(self):
         return self.name
 
     def get_percent(self):
-        return self.rating*100
+        return self.rating / float(self.user_count)
 
 
 class MotelImage(TimeStampedModel):
     motels = models.ForeignKey('motels.Motel', related_name='images')
     image = VersatileImageField(
         'Motels Image',
-        upload_to='uploaded_files/motels/' ,
+        upload_to='uploaded_files/motels/',
         ppoi_field='image_ppoi',
         blank=True
     )
@@ -48,7 +49,7 @@ class MotelImage(TimeStampedModel):
 class Comment(TimeStampedModel):
     motel = models.ForeignKey('motels.Motel', related_name='comments')
     body = models.CharField(max_length=250, unique=False)
-    rating = models.DecimalField(max_digits=2, decimal_places=1, blank=True, null=True)
+    rating = models.FloatField(blank=True, null=True)
     status = models.BooleanField(default=True)
 
     class Meta:
@@ -56,6 +57,14 @@ class Comment(TimeStampedModel):
 
     def __unicode__(self):
         return self.body
+
+    def save(self, *args, **kwargs):
+        motel = self.motel
+        print "Test!!! rating {0}, {1}".format(motel.rating, self.rating)
+        motel.user_count += 1
+        motel.rating += self.rating
+        motel.save()
+        super(Comment, self).save(*args, **kwargs)
 
 
 class Amenitie(TimeStampedModel):
